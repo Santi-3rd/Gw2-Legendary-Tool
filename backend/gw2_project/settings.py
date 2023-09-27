12f13a6e544a99658ead6c8a258928515bd8cb03
Guillermo Santiago <g.santiago3rd@gmail.com>
2023-09-27 13:34:05 -0600

    react setup, and fixed db path

--- backend/gw2_project/settings.py
+++ backend/gw2_project/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
@@ -81,7 +82,7 @@ WSGI_APPLICATION = 'gw2_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'gw2_db',
     }
 }
--- backend/user_app/models.py
+++ backend/user_app/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import AbstractUser
 class App_user(AbstractUser):
     email = models.EmailField(unique=True)
     user_name = models.CharField(max_length=30, unique=True, blank=True, null=True)
-    api_key = models.CharField(max_length=256, unique=True, blank=True, null=True)
     USERNAME_FIELD ='email'
     REQUIRED_FIELDS=[]
+
+class APIKey(models.Model):
+    user = models.ForeignKey(App_user, on_delete=models.CASCADE)
+    key = models.CharField(max_length=256, unique=True)
