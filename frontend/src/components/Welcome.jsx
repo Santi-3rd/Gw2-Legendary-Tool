adbfb6a4836177c8aa23efc4d2daa09628920bca
Guillermo Santiago <g.santiago3rd@gmail.com>
2023-10-06 15:15:03 -0600

    commented out some code

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -5,8 +5,8 @@ import Button from 'react-bootstrap/Button';
 export const Welcome = () => {
 
   return (
-    <div className="bg-slate-600 p-4 border rounded-lg shadow-md text-center mx-auto max-w-md mt-20">
-    <h1 className="text-3xl text-white">Welcome!</h1>
+    <div>
+    <h1 >Welcome!</h1>
     <Button variant="primary">Primary</Button>{' '}
   </div>
 );
