16e584f303531fccb1822e2c617c466ed2b365a4
Guillermo Santiago <g.santiago3rd@gmail.com>
2024-03-12 09:53:39 -0600

    fixed empty table bug

--- frontend/src/components/legendary-type-table.jsx
+++ frontend/src/components/legendary-type-table.jsx
@@ -3,16 +3,20 @@
 export const LegendaryTypeTable = ({filteredItems}) => {
     return (
         <div>
-              <table className="main-table">
-                <tbody>
-                  {filteredItems.map((item, index) => (
-                    <tr key={index} className="main-table">
-                      <td><img src={item.icon}></img></td>
-                      <td className="main-table">{item.name}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+          {filteredItems && filteredItems.length > 0? 
+          <table className="main-table">
+            <tbody>
+              {filteredItems.map((item, index) => (
+                <tr key={index} className="main-table">
+                  <td><img src={item.icon}></img></td>
+                  <td className="main-table">{item.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+              :
+            <></>
+          }
+        </div>
     )
 }
